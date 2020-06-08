38beb96d58198d2fa3c2b4a8e6a9bfbd0137aa4d
Vinod Kumar P V <vinod.kumar2@byjus.com>
2020-06-09 03:45:57 +0530

    first commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,11 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = "4d1272dc552f2f6910ff670a377ff434"
+  config.secret = "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"
+  config.old_secret = "<old_secret>"
+  config.scope = "read_products" # Consult this page for more scope options:
+                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.session_repository = Shop
+end
