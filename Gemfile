967d9936361a13ca7e5c0020f9c4bb0554b7b511
Vinod Kumar P V <vinod.kumar@kritikalvision.ai>
2020-06-09 17:40:49 +0530

    session error shopify

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.5.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'shopify_app'
 
+gem 'shopify_api'
+
 gem 'rails', '~> 5.2.4', '>= 5.2.4.2'
 
 # Use postgresql as the database for Active Record
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -7,6 +7,6 @@ ShopifyApp.configure do |config|
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
-  config.session_repository = Shop
   config.api_version = '2019-04'
+  config.session_repository = ::Shop
 end
